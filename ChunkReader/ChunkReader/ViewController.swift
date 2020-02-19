fdb168477ab63d8ae1cf316841f5ca2f7d11c4e2
Nate de Jager <nate@jumprope.com>
2020-02-19 09:23:16 -0800

    Uploading Coding Challenge Starter.

---
+++ ChunkReader/ChunkReader/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ChunkReader
+//
+//  Created by Nate de Jager on 2020-02-19.
+//  Copyright © 2020 Jumprope. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
+
